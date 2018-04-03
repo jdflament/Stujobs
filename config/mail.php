457a5fd40adca32a80ea4ce165f168a0b9ae7454
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-03 23:51:15 +0200

    wip password reset

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'cryptee.app@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'team.stujobs@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Stujobs'),
     ],
 

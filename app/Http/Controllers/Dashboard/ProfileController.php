18c2a4fe86bced3e99c695f6fbd4cd8e64a6a206
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-04 00:17:13 +0200

    fix first or new

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -57,7 +57,7 @@ class ProfileController extends Controller
         $user->setAttribute('email', $user_data['edit_email']);
         $user->save();
         
-        $admin = Admin::where(['user_id' => $id])->first();        
+        $admin = Admin::firstOrNew(['user_id' => $id]);
         $admin->setAttribute('firstname', $admin_data['edit_firstname']);
         $admin->setAttribute('lastname', $admin_data['edit_lastname']);
         $admin->setAttribute('phone', $admin_data['edit_phone']);

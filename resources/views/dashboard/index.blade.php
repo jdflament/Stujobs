d83e534cb70bd0a8d35e840105a6f37e862173fa
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-08 23:11:51 +0200

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,17 +8,23 @@
                     <div class="card-header">Offres à valider</div>
                     <div class="card-body" style="max-height: 350px; overflow-y: scroll;">
                         <div class="list-group">
-                            @foreach ($offersToValid as $offerToValid)
-                                <a href="/dashboard/offers/{{ $offerToValid->id }}/show" class="list-group-item list-group-item-action flex-column align-items-start">
-                                    <div class="d-flex w-100 justify-content-between" style="display: -webkit-box !important">
-                                        <h5 class="mb-1 text-truncate">{{ $offerToValid->title }} <span style="font-size: 12px">(@lang('vocabulary.' . $offerToValid->contract_type))</span></h5>
+                            @if (count($offersToValid) > 0)
+                                @foreach ($offersToValid as $offerToValid)
+                                    <a href="/dashboard/offers/{{ $offerToValid->id }}/show" class="list-group-item list-group-item-action flex-column align-items-start">
+                                        <div class="d-flex w-100 justify-content-between" style="display: -webkit-box !important">
+                                            <h5 class="mb-1 text-truncate">{{ $offerToValid->title }} <span style="font-size: 12px">(@lang('vocabulary.' . $offerToValid->contract_type))</span></h5>
 
-                                        <small class="badge badge-primary">{{ (new Carbon\Carbon($offerToValid->created_at))->diffForHumans() }}</small>
-                                    </div>
-                                    <p class="mb-1 text-truncate" style="color: inherit;">{{ $offerToValid->description }}</p>
-                                    <small>Posté par {{ $offerToValid->name }}</small>
+                                            <small class="badge badge-primary">{{ (new Carbon\Carbon($offerToValid->created_at))->diffForHumans() }}</small>
+                                        </div>
+                                        <p class="mb-1 text-truncate" style="color: inherit;">{{ $offerToValid->description }}</p>
+                                        <small>Posté par {{ $offerToValid->name }}</small>
+                                    </a>
+                                @endforeach
+                            @else
+                                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <p>Aucune offre en cours à valider.</p>
                                 </a>
-                            @endforeach
+                            @endif
                         </div>
                     </div>
                 </div>

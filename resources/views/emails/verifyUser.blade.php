37798e4d2e34de49fa95ac54cd57ddb921f6f1f9
Unknown <thomas.laigneau.pro@gmail.com>
2018-05-14 23:53:16 +0200

    fix title

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title></title>
+<title>Bienvenue sur Stujobs !</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
